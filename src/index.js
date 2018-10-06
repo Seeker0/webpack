90e22abbee7e709171d387a363bdde46fbfb6c90
Jeffrey Dederick <swift@Jeffreys-MBP.lan1>
2018-10-06 16:57:54 -0400

    complete

---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import _ from "lodash";
+
+const component = () => {
+  let element = document.createElement("div");
+
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  return element;
+};
+
+document.body.appendChild(component());
