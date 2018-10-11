dd16f32fecf427bf7a774e72971441ce8b3804f7
Jeffrey Dederick <swift@Jeffreys-MBP.lan1>
2018-10-11 19:46:26 -0400

    assets managed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 "use strict";
 
 import _ from "lodash";
+import "./style.css";
+import Icon from "./damitall.jpeg";
+import Data from "./data.xml";
 
 const component = () => {
   let element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  let myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
 
   return element;
 };
