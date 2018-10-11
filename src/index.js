cd82b34049d16b2dcb4461f82d8c71a035cb3203
Jeffrey Dederick <swift@Jeffreys-MBP.lan1>
2018-10-11 19:49:44 -0400

    directory cleaned

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 "use strict";
 
 import _ from "lodash";
-import "./style.css";
-import Icon from "./damitall.jpeg";
-import Data from "./data.xml";
 
 const component = () => {
   let element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  let myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
 
   return element;
 };
