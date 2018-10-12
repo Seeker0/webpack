79fa3e769992cf82478a43671084d3e5e7e97bec
Jeffrey Dederick <swift@Jeffreys-MBP.lan1>
2018-10-11 21:11:39 -0400

    module hotted

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import _ from "lodash";
 import printMe from "./print.js";
+import "./styles.css";
 
 const component = () => {
   let element = document.createElement("div");
@@ -17,4 +18,15 @@ const component = () => {
   return element;
 };
 
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept("./print.js", () => {
+    console.log("Accepting the updated printMe module!");
+
+    document.body.removeChild(element);
+    element = component();
+    document.body.appendChild(element);
+  });
+}
